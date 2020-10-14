6d13525f90fc8f5a1061618bb6380d2f426505b9
Travis C. LaGrone <Travis.C.LaGrone@outlook.com>
2020-10-14 14:29:41 -0500

    Define Tclg.Management\Get-EnvPath

---
+++ src/Tclg.Management/EnvPath/Get-EnvPath.ps1
@@ -0,0 +1,25 @@
+function Get-EnvPath {
+    [CmdletBinding(PositionalBinding=$false, DefaultParameterSetName='Split')]
+    [OutputType([string[]], ParameterSetName='Split')]
+    [OutputType([string], ParameterSetName='Raw')]
+    param(
+        [Parameter(ParameterSetName='Split', Position=0)]
+        [Parameter(ParameterSetName='Raw', Position=0)]
+        [System.EnvironmentVariableTarget]
+        [ValidateNotNull()]
+        $Target = [System.EnvironmentVariableTarget]::Process,
+
+        [Parameter(ParameterSetName='Raw', Mandatory)]
+        [switch]
+        $Raw
+    )
+
+    $envPath = [System.Environment]::GetEnvironmentVariable('Path', $Target)
+    if ($Raw) {
+        return $envPath
+    }
+
+    ($envPath -split ([System.IO.Path]::PathSeparator)).ForEach{ $_.Trim() }.Where{ $_ }
+}
+
+Set-Alias -Name 'gep' -Value 'Get-EnvPath'
