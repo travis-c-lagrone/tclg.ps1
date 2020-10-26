981fda0e0fa7c4b8c98cc73fdc5fc1a8eab1f5d8
Travis C. LaGrone <Travis.C.LaGrone@outlook.com>
2020-10-26 11:50:59 -0500

    Improve error messaging for Tclg.Utility\ConvertFrom-Timestamp

--- src/Tclg.Utility/Timestamp/ConvertFrom-Timestamp.ps1
+++ src/Tclg.Utility/Timestamp/ConvertFrom-Timestamp.ps1
@@ -56,46 +56,52 @@ function ConvertFrom-Timestamp {
         $Timestamp
     )
     process {
+        $match = $TimestampRegex.Match($Timestamp)
+        if (-not $match.Success) {
+            Write-Error `
+                -Message "Argument not parseable as a timestamp." `
+                -Category InvalidArgument `
+                -CategoryActivity 'System.Text.RegularExpressions.Regex.Match' `
+                -CategoryReason 'Match failed.' `
+                -CategoryTargetName 'Timestamp' `
+                -CategoryTargetType ([string]) `
+                -TargetObject $Timestamp
+            return
+        }
         try {
-            $match = $TimestampRegex.Match($Timestamp)
-            if ($match.Success) {
-                $groups = $match.Groups
-                return $(
-                    if ($groups['Offset'].Success) {
-                        $datetime = [DateTime]::new(
-                            $groups['Year'].Success       ? [int] $groups['Year'].Value       : 0,
-                            $groups['Month'].Success      ? [int] $groups['Month'].Value      : 0,
-                            $groups['Day'].Success        ? [int] $groups['Day'].Value        : 0,
-                            $groups['Hour'].Success       ? [int] $groups['Hour'].Value       : 0,
-                            $groups['Minute'].Success     ? [int] $groups['Minute'].Value     : 0,
-                            $groups['Second'].Success     ? [int] $groups['Second'].Value     : 0,
-                            $groups['Millsecond'].Success ? [int] $groups['Millsecond'].Value : 0
-                        )
-                        $signum = [int] ($groups['OffsetSign'].Value + '1')
-                        $offset = [TimeSpan]::new(
-                            $groups['OffsetHour'].Success   ? $signum * ([int] $groups['OffsetHour'].Value)   : 0,
-                            $groups['OffsetMinute'].Success ? $signum * ([int] $groups['OffsetMinute'].Value) : 0,
-                            $groups['OffsetSecond'].Success ? $signum * ([int] $groups['OffsetSecond'].Value) : 0
-                        )
-                        [DateTimeOffset]::($datetime, $offset)
-                    }
-                    else {
-                        [DateTime]::new(
-                            $groups['Year'].Success       ? [int] $groups['Year'].Value       : 0,
-                            $groups['Month'].Success      ? [int] $groups['Month'].Value      : 0,
-                            $groups['Day'].Success        ? [int] $groups['Day'].Value        : 0,
-                            $groups['Hour'].Success       ? [int] $groups['Hour'].Value       : 0,
-                            $groups['Minute'].Success     ? [int] $groups['Minute'].Value     : 0,
-                            $groups['Second'].Success     ? [int] $groups['Second'].Value     : 0,
-                            $groups['Millsecond'].Success ? [int] $groups['Millsecond'].Value : 0,
-                            $groups['Universal'].Success ? [DateTimeKind]::Utc : [DateTimeKind]::Unspecified
-                        )
-                    }
-                )
-            }
-            else {
-                Write-Error "The string is not parseable as a timestamp: $Timestamp"
-            }
+            $groups = $match.Groups
+            return $(
+                if ($groups['Offset'].Success) {
+                    $datetime = [DateTime]::new(
+                        $groups['Year'].Success       ? [int] $groups['Year'].Value       : 0,
+                        $groups['Month'].Success      ? [int] $groups['Month'].Value      : 0,
+                        $groups['Day'].Success        ? [int] $groups['Day'].Value        : 0,
+                        $groups['Hour'].Success       ? [int] $groups['Hour'].Value       : 0,
+                        $groups['Minute'].Success     ? [int] $groups['Minute'].Value     : 0,
+                        $groups['Second'].Success     ? [int] $groups['Second'].Value     : 0,
+                        $groups['Millsecond'].Success ? [int] $groups['Millsecond'].Value : 0
+                    )
+                    $signum = [int] ($groups['OffsetSign'].Value + '1')
+                    $offset = [TimeSpan]::new(
+                        $groups['OffsetHour'].Success   ? $signum * ([int] $groups['OffsetHour'].Value)   : 0,
+                        $groups['OffsetMinute'].Success ? $signum * ([int] $groups['OffsetMinute'].Value) : 0,
+                        $groups['OffsetSecond'].Success ? $signum * ([int] $groups['OffsetSecond'].Value) : 0
+                    )
+                    [DateTimeOffset]::($datetime, $offset)
+                }
+                else {
+                    [DateTime]::new(
+                        $groups['Year'].Success       ? [int] $groups['Year'].Value       : 0,
+                        $groups['Month'].Success      ? [int] $groups['Month'].Value      : 0,
+                        $groups['Day'].Success        ? [int] $groups['Day'].Value        : 0,
+                        $groups['Hour'].Success       ? [int] $groups['Hour'].Value       : 0,
+                        $groups['Minute'].Success     ? [int] $groups['Minute'].Value     : 0,
+                        $groups['Second'].Success     ? [int] $groups['Second'].Value     : 0,
+                        $groups['Millsecond'].Success ? [int] $groups['Millsecond'].Value : 0,
+                        $groups['Universal'].Success ? [DateTimeKind]::Utc : [DateTimeKind]::Unspecified
+                    )
+                }
+            )
         }
         catch {
             Write-Error $_
