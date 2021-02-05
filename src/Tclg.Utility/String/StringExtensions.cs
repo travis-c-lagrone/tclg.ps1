8bee34c5f2523367d6ddd193faadd659842fa4a8
Travis C. LaGrone <Travis.C.LaGrone@outlook.com>
2021-02-04 19:05:31 -0600

    Fix incorrect language syntax for comments in Tclg.Utility\StringExtensions.cs

--- src/Tclg.Utility/String/StringExtensions.cs
+++ src/Tclg.Utility/String/StringExtensions.cs
@@ -2,6 +2,6 @@ namespace Tclg.Utility
 {
     public static class StringExtensions
     {
-        # TODO Convert all inline ScriptMethods in String.type.ps1xml to CodeMethods.
+        // TODO Convert all inline ScriptMethods in String.type.ps1xml to CodeMethods.
     }
 }
