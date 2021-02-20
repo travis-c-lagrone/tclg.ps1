3cd241ff70c01f668311602a1c378320b24d35f9
Travis C. LaGrone <Travis.C.LaGrone@outlook.com>
2021-02-20 07:44:21 -0600

    Validate the argument to Tclg.Management\Get-EnvVariable:Target
    Namely, ensure that the argument is a supported value on the runtime platform.

--- src/Tclg.Management/EnvVariable/Get-EnvVariable.ps1
+++ src/Tclg.Management/EnvVariable/Get-EnvVariable.ps1
@@ -37,6 +37,8 @@ function Get-EnvVariable {
         [Parameter(ParameterSetName='ByName')]
         [Alias('EnvironmentVariableTarget')]
         [ValidateNotNull()]
+        [ValidateScript({ $IsWindows -or $_ -eq [EnvironmentVariableTarget]::Process },
+                        ErrorMessage='The environment variable target ''{0}'' is supported only on Windows.')]
         [EnvironmentVariableTarget]
         $Target = [EnvironmentVariableTarget]::Process,
 
