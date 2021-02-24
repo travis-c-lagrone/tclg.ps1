002a1fa1c914a71f94bd7cfd6f34ac288ed0bd9e
Travis C. LaGrone <Travis.C.LaGrone@outlook.com>
2021-02-24 04:44:44 -0600

    Refine naming conventions for clarity
    Changes:
    - "(Remove|Replace)Prefix" => "$1Head"
    - "(Remove|Replace}Suffix" => "$1Tail"
    
    These four methods operate on length-delimited ranges,
    not content-determined substrings.

--- src/Tclg.Utility/String/StringExtensions.cs
+++ src/Tclg.Utility/String/StringExtensions.cs
@@ -12,14 +12,14 @@ namespace Tclg.Utility
         public static string Remove(this string @this, string value) => @this.Replace(value, String.Empty);
         public static string RemoveFirst(this string @this, string value) { int index = @this.IndexOf(value); return index >= 0 ? @this.Remove(index, value.Length) : @this; }
         public static string RemoveLast(this string @this, string value) { int index = @this.LastIndexOf(value); return index >= 0 ? @this.Remove(index, value.Length) : @this; }
-        public static string RemovePrefix(this string @this, int length) => @this.Substring(Math.Clamp(length, 0, @this.Length));
+        public static string RemoveHead(this string @this, int length) => @this.Substring(Math.Clamp(length, 0, @this.Length));
         public static string RemovePrefixIfPresent(this string @this, string value) => @this.StartsWith(value) ? @this.Substring(value.Length) : @this;
-        public static string RemoveSuffix(this string @this, int length) => @this.Substring(0, @this.Length - Math.Clamp(length, 0, @this.Length));
+        public static string RemoveTail(this string @this, int length) => @this.Substring(0, @this.Length - Math.Clamp(length, 0, @this.Length));
         public static string RemoveSuffixIfPresent(this string @this, string value) => @this.EndsWith(value) ? @this.Substring(0, @this.Length - value.Length) : @this;
-        public static string ReplacePrefix(this string @this, int length, string newValue) => @this.RemovePrefix(length).AddPrefix(newValue);
-        public static string ReplacePrefixIfPresent(this string @this, string oldValue, string newValue) => @this.StartsWith(oldValue) ? @this.RemovePrefix(oldValue.Length).AddPrefix(newValue) : @this;
-        public static string ReplaceSuffix(this string @this, int length, string newValue) => @this.RemoveSuffix(length).AddSuffix(newValue);
-        public static string ReplaceSuffixIfPresent(this string @this, string oldValue, string newValue) => @this.EndsWith(oldValue) ? @this.RemoveSuffix(oldValue.Length).AddSuffix(newValue) : @this;
+        public static string ReplaceHead(this string @this, int length, string newValue) => @this.RemoveHead(length).AddPrefix(newValue);
+        public static string ReplacePrefixIfPresent(this string @this, string oldValue, string newValue) => @this.StartsWith(oldValue) ? @this.RemoveHead(oldValue.Length).AddPrefix(newValue) : @this;
+        public static string ReplaceTail(this string @this, int length, string newValue) => @this.RemoveTail(length).AddSuffix(newValue);
+        public static string ReplaceSuffixIfPresent(this string @this, string oldValue, string newValue) => @this.EndsWith(oldValue) ? @this.RemoveTail(oldValue.Length).AddSuffix(newValue) : @this;
         public static string SubstringFrom(this string @this, string value) { int index = @this.IndexOf(value); return index >= 0 ? @this.Substring(index) : String.Empty; }
         public static string SubstringFromLast(this string @this, string value) { int index = @this.LastIndexOf(value); return index >= 0 ? @this.Substring(index) : String.Empty; }
         public static string SubstringUntil(this string @this, string value) { int index = @this.IndexOf(value); return index >= 0 ? @this.Substring(0, index) : String.Empty; }
@@ -37,13 +37,13 @@ namespace Tclg.Utility
         public static string Remove(PSObject @this, string value) => @this.ToString().Remove(value);
         public static string RemoveFirst(PSObject @this, string value) => @this.ToString().RemoveFirst(value);
         public static string RemoveLast(PSObject @this, string value) => @this.ToString().RemoveLast(value);
-        public static string RemovePrefix(PSObject @this, int length) => @this.ToString().RemovePrefix(length);
+        public static string RemoveHead(PSObject @this, int length) => @this.ToString().RemoveHead(length);
         public static string RemovePrefixIfPresent(PSObject @this, string value) => @this.ToString().RemovePrefixIfPresent(value);
-        public static string RemoveSuffix(PSObject @this, int length) => @this.ToString().RemoveSuffix(length);
+        public static string RemoveTail(PSObject @this, int length) => @this.ToString().RemoveTail(length);
         public static string RemoveSuffixIfPresent(PSObject @this, string value) => @this.ToString().RemoveSuffixIfPresent(value);
-        public static string ReplacePrefix(PSObject @this, int length, string newValue) => @this.ToString().ReplacePrefix(length, newValue);
+        public static string ReplaceHead(PSObject @this, int length, string newValue) => @this.ToString().ReplaceHead(length, newValue);
         public static string ReplacePrefixIfPresent(PSObject @this, string oldValue, string newValue) => @this.ToString().ReplacePrefixIfPresent(oldValue, newValue);
-        public static string ReplaceSuffix(PSObject @this, int length, string newValue) => @this.ToString().ReplaceSuffix(length, newValue);
+        public static string ReplaceTail(PSObject @this, int length, string newValue) => @this.ToString().ReplaceTail(length, newValue);
         public static string ReplaceSuffixIfPresent(PSObject @this, string oldValue, string newValue) => @this.ToString().ReplaceSuffixIfPresent(oldValue, newValue);
         public static string SubstringFrom(PSObject @this, string value) => @this.ToString().SubstringFrom(value);
         public static string SubstringFromLast(PSObject @this, string value) => @this.ToString().SubstringFromLast(value);
